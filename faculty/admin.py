acd6fbd6dbfeae660aed414d6a8d985c3951ccca
Amisha Sinha <sinhaamisha09@gmail.com>
2020-06-20 08:37:58 +0530

    changes

--- faculty/admin.py
+++ faculty/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from .models import Student, Room, Hostel, Course, User
 
 # Register your models here.
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = [
+        'user',
+        'student_name',
+        'father_name',
+        'gender',
+        'enrollment_no',
+        'course',
+        'dob',
+        'room',
+        'room_allotted']
+
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ['no', 'name', 'room_type', 'vacant', 'hostel']
+
+
+@admin.register(Hostel)
+class HostelAdmin(admin.ModelAdmin):
+    list_display = ['name', 'gender']
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['code', 'room_type']
+
+
+@admin.register(User)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['is_warden']
